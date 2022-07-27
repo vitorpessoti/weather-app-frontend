b0762ad9c6ec28d15f7842eeab6580f2251e6ee7
wilson6g <wilsonpinheirodev@gmail.com>
2022-07-27 12:23:49 -0300

    Added image in home

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,11 +1,28 @@
 import { BaseLayout } from '../../layouts/BaseLayout';
+import Rain from '../../assets/images/10n.jpg';
+import './styles.scss';
 
 export function HomePage() {
   return (
-    <div className="Home">
+    <>
       <BaseLayout>
-        <h1>Hello home!</h1>
+        <div className="home">
+          <aside className="image-container">
+            <img src={Rain} alt="rain" />
+            <figcaption className="country-state-text">Tokyo, Japan</figcaption>
+            <figcaption className="temperature-text">
+              19°C <figcaption>Rain</figcaption>
+            </figcaption>
+          </aside>
+
+          <div className="data-container">
+            <h1>Hello home!</h1>
+            <h1>Hello home!</h1>
+            <h1>Hello home!</h1>
+            <h1>Hello home!</h1>
+          </div>
+        </div>
       </BaseLayout>
-    </div>
+    </>
   );
 }
