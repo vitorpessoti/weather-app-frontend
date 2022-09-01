afe453b2ffe6ce92c651f9ef53c809312efebac5
Vitor Pessoti <vluiz@iblueconsulting.com.br>
2022-09-01 12:55:02 -0300

    fixing vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: /^@mui\/icons-material\/(.*)/,
+        replacement: "@mui/icons-material/esm/$1"
+      }
+    ]
+  }
 })
